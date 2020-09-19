ade7c8c6ace1308fb051df7955df2b1629f8cf3f
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-19 21:44:34 +0300

    creating sample Coach ap

---
+++ src/com/kumsal/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.kumsal.springdemo;
+
+public class BaseballCoach {
+	public String getDailyWorkout(){
+		return "Spend 30 minute on batting practice";
+	}
+
+}
---
+++ src/com/kumsal/springdemo/MyApp.java
@@ -0,0 +1,9 @@
+package com.kumsal.springdemo;
+
+public class MyApp {
+	public static void main(String[] args) {
+		BaseballCoach theCoach=new BaseballCoach();
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
