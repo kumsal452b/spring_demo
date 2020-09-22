cfe8430b5d9e21b8e1e0449ff56dd0459f625208
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-22 21:12:01 +0300

    Nothing

--- src/com/kumsal/springdemo/HappyFortuneServices.java
+++ src/com/kumsal/springdemo/HappyFortuneServices.java
@@ -1,5 +1,10 @@
 package com.kumsal.springdemo;
 
-public class HappyFortuneServices {
+public class HappyFortuneServices implements FortuneService{
 
+	@Override
+	public String getFortune() {
+	
+		return "Today is your lucky day!";
+	}
 }
