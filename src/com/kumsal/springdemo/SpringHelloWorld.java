d19ab70d38ece8dfc5fb01aa0f7e19877e259bd0
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-22 20:02:14 +0300

    problem fıx ıt

--- src/com/kumsal/springdemo/SpringHelloWorld.java
+++ src/com/kumsal/springdemo/SpringHelloWorld.java
@@ -1,13 +1,23 @@
 package com.kumsal.springdemo;
 
+import java.io.File;
+
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 
 public class SpringHelloWorld {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		File file=new File("C:\\Users\\keski\\Desktop\\github-sample\\spring_demo\\src\\com\\kumsal\\springdemo\\applicationContext.xml");
+		System.out.println(file.exists());
+		System.out.println(file.canRead());
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("file:src/com/kumsal/springdemo/applicationContext.xml") ;
+		context.refresh();
 		Coach theCoach=context.getBean("myCoach",Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
 		context.close();
+			
 	}
 
 }
