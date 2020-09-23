679ef26353b9eb67cba5a15cddd7bae38c0a3b6b
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-23 22:08:26 +0300

    added setter injection

--- src/com/kumsal/springdemo/SetterDemoApp.java
+++ src/com/kumsal/springdemo/SetterDemoApp.java
@@ -1,10 +1,15 @@
 package com.kumsal.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SetterDemoApp {
 
 	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("file:src/com/kumsal/springdemo/applicationContext.xml") ;
+		CricketCoach thecoach=context.getBean("myCricetCoach", CricketCoach.class);
+		System.out.println(thecoach.getDailyFortune());
+		System.out.println(thecoach.getDailyWorkout());
 		
-
 	}
 
 }
