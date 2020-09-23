eb53517f4a94f9820736e4866a453f4c50c90c1a
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-23 21:43:08 +0300

    injection setter method in Spring metod

---
+++ src/com/kumsal/springdemo/CricketCoach.java
@@ -0,0 +1,31 @@
+package com.kumsal.springdemo;
+
+public class CricketCoach implements Coach{
+
+	FortuneService fortuneService;
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	public CricketCoach(){
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
+	
+	public CricketCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Practicte fast bowling for 15 minute";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
+	}
+	
+}
