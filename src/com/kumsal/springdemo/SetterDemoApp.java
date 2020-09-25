459d6550ec66e9a3db7bc985722d88096cfbe893
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-25 22:36:29 +0300

    end of day

--- src/com/kumsal/springdemo/SetterDemoApp.java
+++ src/com/kumsal/springdemo/SetterDemoApp.java
@@ -7,9 +7,11 @@ public class SetterDemoApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("file:src/com/kumsal/springdemo/applicationContext.xml") ;
 		CricketCoach thecoach=context.getBean("myCricketCoach", CricketCoach.class);
-//		System.out.println(thecoach.getDailyFortune());
-//		System.out.println(thecoach.getDailyWorkout());
+		System.out.println(thecoach.getDailyFortune());
+		System.out.println(thecoach.getDailyWorkout());
 		
+		System.out.println(thecoach.getTeam());
+		System.out.println(thecoach.getEmail());
 	}
 
 }
