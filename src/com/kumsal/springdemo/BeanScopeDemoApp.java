4b3de46c1f29e4f9326ae2215e4f8dd3fe8f422f
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-28 20:13:28 +0300

    Nothing

--- src/com/kumsal/springdemo/BeanScopeDemoApp.java
+++ src/com/kumsal/springdemo/BeanScopeDemoApp.java
@@ -7,18 +7,12 @@ public class BeanScopeDemoApp {
 	public static void main(String[] args) {
 //		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("file:src/com/kumsal/springdemo/applicationContext.xml") ;
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
-		
 		Coach theCoach=context.getBean("myCoach",Coach.class);
-		
 		Coach alphaCoach=context.getBean("myCoach",Coach.class);
-		
 		boolean result=(theCoach==alphaCoach);
-		
 		System.out.println("\nPointing to the same Object: "+result);
 		System.out.println("\nMemory location for the coach: "+theCoach);
 		System.out.println("\nMemory location for the coach: "+alphaCoach);
-		
 		context.close();
 	}
-
 }
