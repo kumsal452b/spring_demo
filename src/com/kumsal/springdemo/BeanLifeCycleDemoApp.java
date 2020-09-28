61d4d3888bc035e51cebf35675652692bb8e002c
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-28 20:37:15 +0300

    add new element

--- src/com/kumsal/springdemo/BeanLifeCycleDemoApp.java
+++ src/com/kumsal/springdemo/BeanLifeCycleDemoApp.java
@@ -1,5 +1,14 @@
 package com.kumsal.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class BeanLifeCycleDemoApp {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		Coach theCoach=context.getBean("myCoach",TrackCoach.class);
+		
+		
+		System.out.println(theCoach.getDailyWorkout());
+	}
 
 }
